f89b0f009266e14c8d44dc9a44d4544c484dd2bd
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-01-30 10:53:07 +0000

    started worsksheet 2.1

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,6 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+
+SELECT DepartmentName, Count(TotalEnrolments)
+FROM department
