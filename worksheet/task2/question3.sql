573038479e6f9b2071bfa804d6d423c6402293cc
SawyerCowley <xmgj0858@leeds.ac.uk>
2026-02-01 18:34:16 +0000

    question 5 remaining

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -2,5 +2,8 @@
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
 
-SELECT DepartmentName, Count(TotalEnrolments)
-FROM department
+SELECT d.DepartmentName, Count(*) AS TotalEnrolments
+FROM Department d
+JOIN Course c ON d.DepartmentId = c.DepartmentId
+JOIN Enrolment e ON c.CourseId = e.CourseId
+GROUP BY d.DepartmentName
--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,3 +1,8 @@
 -- List all students along with the names of the courses they are enrolled in.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
+
+SELECT s.StudentId, s.FirstName, s.LastName, c.CourseName
+FROM Student AS s
+JOIN Enrolment AS e ON s.StudentId=e.StudentId
+JOIN Course c ON e.CourseId=c.CourseId
